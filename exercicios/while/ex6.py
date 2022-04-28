100faeabb4aeabfa4a2bc3d9e7b63f86d206c997
othinas <thiagomartinsprado@gmail.com>
2022-04-28 14:52:53 -0300

    Correção no exercício 6 e temporizador feito

--- exercicios/while/ex6.py
+++ exercicios/while/ex6.py
@@ -4,25 +4,26 @@ precoTotal = 0
 credito = float(input('Crédito disponível: '))
 compra = True
 while compra:
-    if precoTotal == credito:
+    if credito == 0:
         break
     else:
         i += 1
         precoUnitario = float(input(f'Preço do item {i}: '))
         if precoUnitario != -1:
-            if precoUnitario <= credito and precoTotal + precoUnitario <= credito:
+            if precoUnitario <= credito:
                 precoTotal += precoUnitario
+                credito -= precoUnitario
+                print(f"Item adicionado. Crédito restante: R${credito:.2f}")
             else:
                 i -= 1
                 print('-------------------------------------')
                 print('Limite excedido, item não adicionado.')
-                print(f'Crédito restante: R${credito - precoTotal:.2f}')
+                print(f'Crédito restante: R${credito:.2f}')
                 print('-------------------------------------')
         else:
             compra = False
 print('-------------------------------------')
 print("Limite atingido. Terminando compra...") if precoTotal == credito else print("Terminando compra...")
 print(f'Preço total: R${precoTotal:.2f}')
-credito -= precoTotal
 print(f'Crédito restante: R${credito:.2f}')
 print('-------------------------------------')
---
+++ exercicios/while/temporizador.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+horaAlarme = int(input("Hora: "))
+minutoAlarme = int(input("Minuto: "))
+segundoAlarme = int(input("Segundo: "))
+
+hora = 0
+while hora < 24:
+    minuto = 0
+    while minuto < 60:
+        segundo = 0
+        while segundo < 60:
+            print(f'{hora:02}:{minuto:02}:{segundo:02}')
+            sleep(1)
+            if hora == horaAlarme and minuto == minutoAlarme and segundo == segundoAlarme:
+                print("BIP-BIP")
+                break
+            segundo += 1
+        if hora == horaAlarme and minuto == minutoAlarme and segundo == segundoAlarme:
+            break 
+        minuto += 1
+    if hora == horaAlarme and minuto == minutoAlarme and segundo == segundoAlarme:
+        break
+    hora += 1
+print("Dia encerrado.")
