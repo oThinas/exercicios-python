753301033407242d794ba3fb1e9ea699abef3ea2
othinas <thiagomartinsprado@gmail.com>
2022-05-26 14:55:10 -0300

    Melhoria na exibição dos dados pro usuário

--- exercicios/empresa.py
+++ exercicios/empresa.py
@@ -1,12 +1,15 @@
+import os
+os.system('cls' if os.name=='nt' else 'clear')
+
 votos = []
 votosTotal = 0
 votosPorcentagem = []
-sistemaOperacional = ["Windows Server: ", "Unix: ", "Linux: ", "Netware: ", "macOS: ", "Outros: "]
+sistemaOperacional = ["Windows Server", "Unix", "Linux", "Netware", "macOS", "Outros"]
 
-print("Digite a quantidade de votos da equete")
+print("Digite a quantidade de votos da enquete")
 
 for i in sistemaOperacional:
-  aux = int(input(i))
+  aux = int(input(f'{i}: '))
   votos.append(aux)
   votosTotal += aux
 
@@ -14,11 +17,14 @@ for i in range(len(votos)):
   aux = (100 * votos[i]) / votosTotal
   votosPorcentagem.append(aux)
   
-print("Sistema operacional | Votos | %")
+print("Resultado da Enquete:")
+print("Sistema operacional | Votos | Porcentagem")
 for i in range(len(votos)):
-  print(f'{sistemaOperacional[i]} | {votos[i]} | {votosPorcentagem[i]:.1f}')
-print("Total: ", votosTotal)
+  espacosNome = " " * (len("Sistema operacinal |") - len(sistemaOperacional[i]))
+  espacosVoto = " " * (len("Votos ") - len(str(votos[i])))
+  print(f'{sistemaOperacional[i]}{espacosNome}| {votos[i]}{espacosVoto}| {round(votosPorcentagem[i])}%')
+print(f'Total: {votosTotal}')
 
 
 aux = votos.index(max(votos))
-print(f'O Sistema Operacional mais votado foi o {sistemaOperacional[aux]} com {max(votos)} votos, correspondendo a {max(votosPorcentagem)}% dos votos')
+print(f'O Sistema Operacional mais votado foi o {sistemaOperacional[aux]} com {max(votos)} votos, correspondendo a {round(max(votosPorcentagem))}% dos votos')
